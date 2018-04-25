67797a46484efcc828f17ca9bc95037cb6380a31
youzhanghao <youzhanghao@ennew.cn>
2018-04-25 15:36:22 +0800

    初始化spring boot项目

---
+++ src/main/java/club/iyzh/log4jelkdemo/Log4jElkDemoApplication.java
@@ -0,0 +1,12 @@
+package club.iyzh.log4jelkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jElkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4jElkDemoApplication.class, args);
+	}
+}
